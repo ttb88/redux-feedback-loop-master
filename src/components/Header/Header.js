4feccf9fd1468162e5771e5efa665d702e0c05f1
Tony Braasch <tonybraasch@gmail.com>
2019-03-04 08:52:29 -0600

    add alt tag to header icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
                 <div className="App-title">
                     <div className='header-text-icon-div'>
                         {/* <img className="header-icon" src="/images/feedback_white.svg" /> */}
-                        <img className="header-icon" src="/images/feedback_2.svg" />
+                        <img className="header-icon" alt="comment icon" src="/images/feedback_2.svg" />
                         <h1>Feedback!</h1>
                     </div>
                         <h4>Don't forget it!</h4>
